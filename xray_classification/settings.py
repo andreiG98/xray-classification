ac6ab7bf1ca947eec4830ef0c57a6cc0848e9ddd
Andrei-Cristian Gidea <andrei.gadea@yahoo.com>
2020-05-14 22:31:04 +0300

    Updated settings

--- xray_classification/settings.py
+++ xray_classification/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0_6j=*xhg%vhn!+2gz$r430u90!x1zs&oi5&no5czln&jkb6i^'
+# SECRET_KEY = '0_6j=*xhg%vhn!+2gz$r430u90!x1zs&oi5&no5czln&jkb6i^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://xray-classification-production.herokuapp.com']
 
 
 # Application definition
